b791bad5575e1327f1ce0ed54d52d98b8ffed197
Your Name <you@example.com>
2022-09-28 11:02:25 +0530

    added:Password having atleast 1 special character

--- src/com/junit/UserRegistration.java
+++ src/com/junit/UserRegistration.java
@@ -48,7 +48,8 @@ public class UserRegistration {
     public static boolean password(String password) {
         //String pattern = "[a-zA-Z0-9]{8,}$";
         //String pattern = "^[A-Z]{1}[a-zA-Z0-9]{7,}$";
-        String pattern = "^[A-Z]{1}+[a-zA-z1-9]{6,}[1-9]{1}$";
+        //String pattern = "^[A-Z]{1}+[a-zA-z1-9]{6,}[1-9]{1}$";
+        String pattern = "(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}";
         if (password.matches(pattern)) {
             return true;
         }
