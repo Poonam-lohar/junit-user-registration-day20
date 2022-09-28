7896d42e3b0abbf21df5860aad38866b0a8b98e4
Your Name <you@example.com>
2022-09-28 10:50:59 +0530

    added:Password having minimum 8 characters

--- src/com/junit/UserRegistration.java
+++ src/com/junit/UserRegistration.java
@@ -15,6 +15,9 @@ public class UserRegistration {
         String Email = mail.nextLine();
         System.out.println("Enter your Mobile Number:");
         String MobNo = scan.nextLine();
+        System.out.println("Enter password:");
+        String password = scan.nextLine();
+
         System.out.println("first name is : " + firstName);
         System.out.println("first name is : " + firstName(firstName));
         System.out.println("first name is : " + lastName);
@@ -23,17 +26,30 @@ public class UserRegistration {
         System.out.println("EmailID is : " + Email(Email));
         System.out.println("mobile number is : " + MobNo);
         System.out.println("mobile number is : " + MobNo(MobNo));
+        System.out.println("Password is " + password(password));
     }
-        private static boolean firstName (String firstName){
-            return firstName.matches("[A-Z]{1}[a-z]{3,}");
-        }
-        private static boolean lastName(String lastName) {
+
+    private static boolean firstName(String firstName) {
+        return firstName.matches("[A-Z]{1}[a-z]{3,}");
+    }
+
+    private static boolean lastName(String lastName) {
         return lastName.matches("[A-Z]{1}[a-z]{3,}");
     }
+
     public static boolean Email(String Email) {
         return Email.matches("^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?");
     }
+
     private static boolean MobNo(String MobNo) {
         return MobNo.matches("^(91)\\s{1}[7-9]{1}[0-9]{9}$");
     }
+
+    public static boolean password(String password) {
+        String pattern = "[a-zA-Z0-9]{8,}$";
+        if (password.matches(pattern)) {
+            return true;
+        }
+        return false;
     }
+}
