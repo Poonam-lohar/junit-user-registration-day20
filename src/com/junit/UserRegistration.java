a6754912203034a73e3da507cec418813b086871
Your Name <you@example.com>
2022-09-28 10:26:08 +0530

    added:enter last name

--- src/com/junit/UserRegistration.java
+++ src/com/junit/UserRegistration.java
@@ -8,12 +8,17 @@ public class UserRegistration {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your first name : ");
         String firstName = scan.nextLine();
+        System.out.println("Enter last name:  ");
+        String lastName = scan.nextLine();
         System.out.println("first name is : " + firstName);
         System.out.println("first name is : " + firstName(firstName));
-
+        System.out.println("first name is : " + lastName);
+        System.out.println("first name is : " + lastName(lastName));
     }
         private static boolean firstName (String firstName){
             return firstName.matches("[A-Z]{1}[a-z]{3,}");
         }
-
+        private static boolean lastName(String lastName) {
+        return lastName.matches("[A-Z]{1}[a-z]{3,}");
+    }
     }
