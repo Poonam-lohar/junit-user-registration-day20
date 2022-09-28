a0ecfd88a79388d2b8e9d7357863b771107adcf0
Your Name <you@example.com>
2022-09-28 10:41:41 +0530

    added:enter mobile number

--- src/com/junit/UserRegistration.java
+++ src/com/junit/UserRegistration.java
@@ -13,12 +13,16 @@ public class UserRegistration {
         Scanner mail = new Scanner(System.in);
         System.out.println("Enter your email : ");
         String Email = mail.nextLine();
+        System.out.println("Enter your Mobile Number:");
+        String MobNo = scan.nextLine();
         System.out.println("first name is : " + firstName);
         System.out.println("first name is : " + firstName(firstName));
         System.out.println("first name is : " + lastName);
         System.out.println("first name is : " + lastName(lastName));
         System.out.println("EmailID is : " + Email);
         System.out.println("EmailID is : " + Email(Email));
+        System.out.println("mobile number is : " + MobNo);
+        System.out.println("mobile number is : " + MobNo(MobNo));
     }
         private static boolean firstName (String firstName){
             return firstName.matches("[A-Z]{1}[a-z]{3,}");
@@ -28,5 +32,8 @@ public class UserRegistration {
     }
     public static boolean Email(String Email) {
         return Email.matches("^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?");
+    }
+    private static boolean MobNo(String MobNo) {
+        return MobNo.matches("^(91)\\s{1}[7-9]{1}[0-9]{9}$");
     }
     }
